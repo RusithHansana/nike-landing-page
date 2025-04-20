76efb61e1c3cf3f1a4febe911e31ea5c961e9a80
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2025-04-20 19:53:19 +0530

    feat: add responsive hero section removed unnecessary imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { default as Nav } from "./Nav.jsx";
+export { default as Button } from "./Button.jsx";
+export { default as ShoeCard } from "./ShoeCard.jsx";
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,36 @@
+import {
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3,
+} from "../assets/images";
+
 export const navLinks = [
   { href: "#home", label: "Home" },
   { href: "#about-us", label: "About Us" },
   { href: "#products", label: "Products" },
   { href: "#contact-us", label: "Contact Us" },
+];
+
+export const shoes = [
+  {
+    thumbnail: thumbnailShoe1,
+    shoe: bigShoe1,
+  },
+  {
+    thumbnail: thumbnailShoe2,
+    shoe: bigShoe2,
+  },
+  {
+    thumbnail: thumbnailShoe3,
+    shoe: bigShoe3,
+  },
+];
+
+export const statistics = [
+  { value: "1K+", label: "Brands" },
+  { value: "500+", label: "Shops" },
+  { value: "250K+", label: "Customers" },
 ];
